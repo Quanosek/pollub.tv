32578f9a2e7b9eaaa04223e2e621d10db0e64718
Quanosek <kubaklalo@gmail.com>
2022-04-12 11:54:15 +0200

    oprawki wizualne

--- functions/msgAutoDelete.js
+++ functions/msgAutoDelete.js
@@ -7,15 +7,15 @@ const realDate = require('./realDate.js')
 
 function msgAutoDelete(msg, delay) {
 
-    if (!msg) console.log(`> ` + clr.brightCyan(`[${realDate()}]`) + ` Guild: ${msg.guild.name}, ${msg.guild.id}\n>> On msgAutoDelete: ` + clr.Red(`'msg' is not declared`) + `!`);
+    if (!msg) console.log(realDate() + ` Guild: ${msg.guild.name}, ${msg.guild.id}\n >>> On msgAutoDelete: ` + clr.red(`'msg' is not declared`) + `!`);
 
     if (!delay) delay = 10 // delay of deleting
 
-    delay += '000' // seconds to miliseconds
+    delay += '000' // to milliseconds
 
     setTimeout(() => msg.delete().catch(err => {
         if (err.code !== 10008) {
-            console.error(`> ` + clr.brightCyan(`[${realDate()}]`) + ` Guild: ${msg.guild.name}, ${msg.guild.id}\n>> On msgAutoDelete: ` + clr.Red(`Failed to delete the message (code ${err.code})`) + `!`);
+            console.error(realDate() + ` Guild: ${msg.guild.name}, ${msg.guild.id}\n >>> On msgAutoDelete: ` + clr.red(`Failed to delete the message (code ${err.code})`) + `!`);
         }
     }), delay)
 
